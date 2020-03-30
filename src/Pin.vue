a665a8b3fdc2ff4ff5c2401a7463c0d8b9051aa2
ViktorFurdakov <vfurdakov@aitheon.com>
2020-03-30 14:16:08 +0300

    Code format

--- src/Pin.vue
+++ src/Pin.vue
@@ -1,72 +1,72 @@
 <template lang="pug">
-.pin(
-  :style="{left: pin.x+'px', top: pin.y+'px'}"
-  @pointerdown="down"
-  @pointerup="pinup"
-)
+    .pin(
+        :style="{left: pin.x+'px', top: pin.y+'px'}"
+        @pointerdown="down"
+        @pointerup="pinup"
+    )
 </template>
 
 
 <script>
-const State = { PICKED: 0, MOVED: 1, DROPED: 2}
+  const State = { PICKED: 0, MOVED: 1, DROPED: 2 }
 
-export default {
-  props: ['pin', 'change', 'remove'],
-  inject: ['editor', 'connection'],
-  data() {
-    return {
-      state: State.DROPED
-    }
-  },
-  mounted() {
-    window.addEventListener('pointermove', this.move);
-    window.addEventListener('pointerup', this.up);
-  },
-  destroyed() {
-    window.removeEventListener('pointermove', this.move);
-    window.removeEventListener('pointerup', this.up);
-  },
-  methods: {
-    setPosition(x, y) {
-      this.$emit('change', {x, y});
-      this.$forceUpdate();
+  export default {
+    props: ['pin', 'change', 'remove'],
+    inject: ['editor', 'connection'],
+    data() {
+      return {
+        state: State.DROPED
+      }
     },
-    down(e){
-      e.stopPropagation();
-      this.state = State.PICKED;
+    mounted() {
+      window.addEventListener('pointermove', this.move);
+      window.addEventListener('pointerup', this.up);
     },
-    move(e){
-      if(this.state === State.DROPED) return;
-
-      this.state = State.MOVED;
-      e.preventDefault();
-
-      const { mouse } = this.editor.view.area;
-
-      this.setPosition(mouse.x, mouse.y);
+    destroyed() {
+      window.removeEventListener('pointermove', this.move);
+      window.removeEventListener('pointerup', this.up);
     },
-    up(e) {
-      this.state = State.DROPED;
-    },
-    pinup() {
-      if(this.state === State.MOVED) return;
+    methods: {
+      setPosition(x, y) {
+        this.$emit('change', { x, y });
+        this.$forceUpdate();
+      },
+      down(e) {
+        e.stopPropagation();
+        this.state = State.PICKED;
+      },
+      move(e) {
+        if (this.state === State.DROPED) return;
 
-      this.$emit('remove', this.pin);
+        this.state = State.MOVED;
+        e.preventDefault();
+
+        const { mouse } = this.editor.view.area;
+
+        this.setPosition(mouse.x, mouse.y);
+      },
+      up(e) {
+        this.state = State.DROPED;
+      },
+      pinup() {
+        if (this.state === State.MOVED) return;
+
+        this.$emit('remove', this.pin);
+      }
     }
   }
-}
 </script>
 
 
 <style lang="sass" scoped>
-$size: 20px
+    $size: 20px
 
-.pin
-  position: absolute
-  display: inline
-  width: $size
-  height: $size
-  background: lighten(steelblue, 20%)
-  border-radius: 50%
-  transform: translate(-50%, -50%)
+    .pin
+        position: absolute
+        display: inline
+        width: $size
+        height: $size
+        background: lighten(steelblue, 20%)
+        border-radius: 50%
+        transform: translate(-50%, -50%)
 </style>
--- src/Pins.vue
+++ src/Pins.vue
@@ -1,33 +1,33 @@
 <template lang="pug">
-div
-  Pin(v-for="pin in pins" :pin="pin"
-    @change="change(pin, $event)"
-    @remove="remove(pin)"
-    :key="pin.x + ' ' + pin.y"
-  )
+    div
+        Pin(v-for="pin in pins" :pin="pin"
+            @change="change(pin, $event)"
+            @remove="remove(pin)"
+            :key="pin.x + ' ' + pin.y"
+        )
 </template>
 
 
 <script>
-import Pin from './Pin.vue';
+  import Pin from './Pin.vue';
 
-export default {
-  props: ['pins'],
-  inject: ['editor', 'connection'],
-  methods: {
-    change(pin, {x, y}) {
-      pin.x = x;
-      pin.y = y;
-      this.editor.view.connections.get(this.connection).update();
+  export default {
+    props: ['pins'],
+    inject: ['editor', 'connection'],
+    methods: {
+      change(pin, { x, y }) {
+        pin.x = x;
+        pin.y = y;
+        this.editor.view.connections.get(this.connection).update();
+      },
+      remove(pin) {
+        this.pins.splice(this.pins.indexOf(pin), 1);
+        this.editor.view.connections.get(this.connection).update();
+        this.$forceUpdate();
+      }
     },
-    remove(pin) {
-      this.pins.splice(this.pins.indexOf(pin), 1);
-      this.editor.view.connections.get(this.connection).update();
-      this.$forceUpdate();
+    components: {
+      Pin
     }
-  },
-  components: {
-    Pin
   }
-}
 </script>
